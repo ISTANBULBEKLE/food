88d0986a063b4f348a2e6e750ccd36e4bed88016
Ekip Kalir <ekipkalir@gmail.com>
2021-09-07 22:20:52 +0100

    quick fix

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,6 +4,10 @@ import ResultsDetails from './ResultsDetails';
 import { withNavigation } from 'react-navigation';
 
 const ResultsList = ({title, results, navigation}) => {
+    
+    if(!results.length){
+        return null;
+    }
 
     return (
         <View style={styles.container}>
